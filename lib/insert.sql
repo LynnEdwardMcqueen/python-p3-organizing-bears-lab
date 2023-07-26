8d35496563ff8a82bf3c9d25f7fddc3b6c951bc1
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-07-26 08:10:04 -0600

    Adds the SQL queries and insertions to service the bear database needs!

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex, alive) VALUES ("Mr. Chocolate", "M", 0);
+INSERT INTO bears (name, sex, alive) VALUES ("Rowdy", "M", 1);
+INSERT INTO bears (name, sex, alive) VALUES ("Tabitha", "F", 1);
+INSERT INTO bears (name, sex, alive) VALUES ("Sergeant Brown", "M", 0);
+INSERT INTO bears (name, sex, alive) VALUES ("Melissa", "F", 1);
+INSERT INTO bears (name, sex, alive) VALUES ("Grinch", "M", 1);
+INSERT INTO bears (name, sex, alive) VALUES ("Wendy", "F", 1);
+INSERT INTO bears (name, sex, alive) VALUES (NULL, "M", 0);
+
